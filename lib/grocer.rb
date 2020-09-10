f813fa89ebfddf5fec1362a93827e58ec8546369
Liam Hanafee-Areces <liamhanafee@gmail.com>
2020-09-10 17:21:31 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,10 @@
 def find_item_by_name_in_collection(name, collection)
   index = 0
   while index < collection.length do
-    return collection[index] if collection[index][:item]== :item
-  end
+    return collection[index] if collection[index][:item] == name 
+  index += 1
+end
+end
   
   # Implement me first!
   #
