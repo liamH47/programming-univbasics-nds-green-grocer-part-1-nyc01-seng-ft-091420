48f30ff22d1103e6ad561015c19ac3ea91c6b399
Liam Hanafee-Areces <liamhanafee@gmail.com>
2020-09-10 17:30:21 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,7 +11,7 @@ def consolidate_cart(cart)
   updated_cart = []
   
   while index < cart.length do 
-    item = find_item_by_name_in_collection(cart,)
+    item = find_item_by_name_in_collection(cart[index][:item], updated_cart)
   
   # Consult README for inputs and outputs
   #
